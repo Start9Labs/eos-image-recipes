2da1a886c369c6927efa9da5e2e5ae8885bce4a7
Matthias Klumpp <matthias@tenstral.net>
2021-03-06 06:56:05 +0100

    Add helper for building images locally

--- README.md
+++ README.md
@@ -3,5 +3,13 @@
 Code and `debos` recipes that are used to create the PureOS live and installer
 images.
 
-Build an image by running the `build.sh` script, or manually by directly calling Debos
-via `debos -m5G --scratchsize=10G -c4 pureos-isohybrid.yaml`.
+If you want to build a local image in the exact same environment used to build
+official PureOS images, you can use the `run-local-build.sh` helper script:
+```bash
+# Prerequisites
+sudo apt install debspawn
+sudo mkdir -p /etc/debspawn/ && echo "AllowUnsafePermissions=true" | sudo tee /etc/debspawn/global.toml
+
+# Build byzantium-gnome-live image (the default)
+./run-local-build.sh byzantium
+```
---
+++ run-local-build.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -e
+
+#
+# This script can be used to safely build an image locally,
+# without the Laniakea Spark runner, but in the exact same
+# environment as if it was executed on the PureOS' build
+# infrastructure.
+#
+
+if [ -z "$1" ]; then
+	echo "usage: $0 SUITE [ENVIRONMENT] [IMAGE-STYLE] [ARCH]"
+	exit 1
+fi
+
+BASEDIR=$(dirname "$(readlink -f "$0")")
+SUITE="$1"
+ENVNAME="$2"
+STYLE="$3"
+ARCH="$4"
+
+imgbuild_fname="$(mktemp /tmp/exec-mkimage.XXXXXX)"
+cat > $imgbuild_fname <<END
+#!/bin/sh
+
+export IB_ENVIRONMENT=${ENVNAME}
+export IB_SUITE=${SUITE}
+export IB_TARGET_ARCH=${ARCH}
+export IB_IMAGE_STYLE=${STYLE}
+./build.sh
+END
+
+mkdir -p ${BASEDIR}/results
+set +e
+debspawn run \
+	-x \
+	--arch=amd64 \
+	--allow=kvm \
+	--cachekey="${SUITE}-mkimage" \
+	--init-command="${BASEDIR}/prepare.sh" \
+	--build-dir=${BASEDIR} \
+	--artifacts-out=${BASEDIR}/results \
+	--header="PureOS Image Build (for ${SUITE})" \
+	${SUITE} \
+	${imgbuild_fname}
+
+# cleanup
+retval=$?
+rm $imgbuild_fname
+if [ $retval -ne 0 ]; then
+    exit $retval
+fi
+exit 0
