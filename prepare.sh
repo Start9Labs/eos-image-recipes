a472e347c23ebafdc3abc267cb31449ab421ad58
Matthias Klumpp <matthias@tenstral.net>
2021-06-06 03:59:28 +0200

    Drop UML support code

--- prepare.sh
+++ prepare.sh
@@ -15,11 +15,3 @@ apt-get install -yq \
 	grub-pc-bin \
 	xorriso \
 	zsync
-
-# in case KVM is unavailable, we need UML
-. /etc/os-release
-if [ "$VERSION_CODENAME" != "amber" ]; then
-    apt-get install -yq \
-	user-mode-linux \
-	libslirp-helper
-fi;
