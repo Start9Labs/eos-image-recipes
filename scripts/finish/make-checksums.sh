d894ddfd547024b00adf61007f55b55505a12b08
Matthias Klumpp <matthias@tenstral.net>
2021-07-05 03:16:37 +0200

    Give checksum files more unique names

--- scripts/finish/make-checksums.sh
+++ scripts/finish/make-checksums.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-RESULTS_DIR=$1
+IMAGE_NAME=$1
+RESULTS_DIR=$2
 
 # write checksum files
 cd $RESULTS_DIR
-find -type f \( -not -name "checksums.*.txt" \) -exec sha256sum '{}' \; > checksums.sha256.txt
-find -type f \( -not -name "checksums.*.txt" \) -exec b2sum '{}' \; > checksums.blake2.txt
+find -type f \( -not -name "$IMAGE_NAME.checksums_*" \) -exec sha256sum '{}' \; > $IMAGE_NAME.checksums_sha256.txt
+find -type f \( -not -name "$IMAGE_NAME.checksums_*" \) -exec b2sum '{}' \; > $IMAGE_NAME.checksums_blake2.txt
