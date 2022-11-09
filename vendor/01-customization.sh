36495d497b9e84b4f21cd0cce85e9300fc42bd0d
Aiden McClelland <me@drbonez.dev>
2022-11-09 13:16:53 -0700

    use local deb

--- vendor/01-customization.sh
+++ vendor/01-customization.sh
@@ -3,14 +3,9 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -LO https://github.com/Start9Labs/embassy-os-deb/releases/download/0.3.2-testing/embassyos_0.3.2-1_amd64.deb
-dpkg -i embassyos_0.3.2-1_amd64.deb || true
-rm embassyos_0.3.2-1_amd64.deb
-
 apt-get update
-apt-get install -yf
-    
-echo embassy > /etc/hostname
+apt install -y /root/embassyos_0.3.2-1_amd64.deb
+rm /root/embassyos_0.3.2-1_amd64.deb
 
 useradd --shell /bin/bash -m start9
 echo start9:embassy | chpasswd
