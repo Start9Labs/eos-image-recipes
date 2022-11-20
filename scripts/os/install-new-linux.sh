7b677e14c2a486c71dfea03df52eceb0ddbefdd9
BluJ <mogulslayer@gmail.com>
2022-11-20 15:19:53 -0700

    chore: Update the image and use apt.

--- scripts/os/install-new-linux.sh
+++ scripts/os/install-new-linux.sh
@@ -2,4 +2,4 @@
 
 cd /tmp
 wget http://ftp.debian.org/debian/pool/main/l/linux/linux-image-6.0.0-0.deb11.2-amd64-dbg_6.0.3-1~bpo11+1_amd64.deb
-dpkg -i linux-image-6.0.0-0.deb11.2-amd64-dbg_6.0.3-1~bpo11+1_amd64.deb
+apt install ./linux-image-6.0.0-0.deb11.2-amd64-dbg_6.0.3-1~bpo11+1_amd64.deb 
