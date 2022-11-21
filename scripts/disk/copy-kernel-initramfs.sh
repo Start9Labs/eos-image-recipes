58741f5717918ebb6066114a55de132e54bc4f44
Aiden McClelland <me@drbonez.dev>
2022-11-21 15:28:35 -0700

    hardcode kernel version

--- scripts/disk/copy-kernel-initramfs.sh
+++ scripts/disk/copy-kernel-initramfs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-cp $ROOTDIR/boot/vmlinuz-* \
+cp $ROOTDIR/boot/vmlinuz-6.0.0-4-amd64 \
     $ARTIFACTDIR/disk-ws-tmp/contents/casper/vmlinuz
-cp $ROOTDIR/boot/initrd.img-* \
+cp $ROOTDIR/boot/initrd.img-6.0.0-4-amd64 \
     $ARTIFACTDIR/disk-ws-tmp/contents/casper/initrd.img
--- scripts/os/install-new-linux.sh
+++ scripts/os/install-new-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /tmp
-wget http://ftp.debian.org/debian/pool/main/l/linux/linux-image-6.0.0-0.deb11.2-amd64-dbg_6.0.3-1~bpo11+1_amd64.deb
-apt install ./linux-image-6.0.0-0.deb11.2-amd64-dbg_6.0.3-1~bpo11+1_amd64.deb 
+wget http://ftp.us.debian.org/debian/pool/main/l/linux-signed-amd64/linux-image-6.0.0-4-amd64_6.0.8-1_amd64.deb
+apt install ./linux-image-6.0.0-4-amd64_6.0.8-1_amd64.deb 
