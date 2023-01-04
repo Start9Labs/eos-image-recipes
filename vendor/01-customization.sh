c9e6dfc25131cb8d9f4aa8c639d88a0c78690416
Aiden McClelland <me@drbonez.dev>
2023-01-04 16:09:15 -0700

    use ./

--- vendor/01-customization.sh
+++ vendor/01-customization.sh
@@ -5,9 +5,11 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 
+modprobe configfs
+
 wget https://downloads.nestybox.com/sysbox/releases/v0.5.2/sysbox-ce_0.5.2-0.linux_amd64.deb
 sha256sum sysbox-ce_0.5.2-0.linux_amd64.deb | grep f13fc0e156f72c6f8bd48e206c59482f83f19acc229701c74e0f23baafa724d8
-apt install -y sysbox-ce_0.5.2-0.linux_amd64.deb
+apt install -y ./sysbox-ce_0.5.2-0.linux_amd64.deb
 rm sysbox-ce_0.5.2-0.linux_amd64.deb
 
 apt install -y /root/embassyos_0.3.x-1_amd64.deb
